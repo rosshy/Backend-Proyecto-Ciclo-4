412093e78c41f6e92555d81530890165efa54f7c
Rosmy Gaviria <roslore55@gmail.com>
2021-12-01 18:04:08 -0500

    actualizacion

--- src/graphQL/Resolvers.js
+++ src/graphQL/Resolvers.js
@@ -1,4 +1,9 @@
 import { resolversProject } from "../models/project/ResolversProject";
 import { resolversUser } from "../models/users/ResolversUser";
+import {resolversInscripciones} from '../models/inscripcion/Resolver.js';
+import {resolversAvance} from '../models/avance/Resolvers.js';
+import { resolversAutenticacion } from './auth/Resolvers.js';
 
-export const resolvers = [ resolversProject, resolversUser ];
+
+
+export const resolvers = [ resolversProject, resolversUser, resolversInscripciones, resolversAvance, resolversAutenticacion ];
--- src/graphQL/TypeDefs.js
+++ src/graphQL/TypeDefs.js
@@ -4,9 +4,10 @@ import { typeUser } from "../models/users/TypesUser";
 import { tiposInscripcion } from '../models/inscripcion/tipos';
 import { tiposEnums } from '../models/enums/tipos.js';
 import { tiposAvance } from '../models/avance/tipos.js';
+import { tiposAutenticacion } from './auth/tipos.js';
 
 const allTypes = gql`
     scalar Date
 `;
 
-export const typesDefs = [ allTypes, typeProject, typeUser, tiposInscripcion, tiposEnums, tiposAvance ];
+export const typesDefs = [ allTypes, typeProject, typeUser, tiposInscripcion, tiposEnums, tiposAvance,  tiposAutenticacion ];
