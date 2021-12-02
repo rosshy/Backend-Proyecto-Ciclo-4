ce047bac85dd4e248e94e138637b27b1296ff991
Rosmy Gaviria <roslore55@gmail.com>
2021-12-02 11:08:07 -0500

    Se realiza ajuste a inscripciones para adicionar estudiante

--- src/graphQL/Resolvers.js
+++ src/graphQL/Resolvers.js
@@ -2,5 +2,5 @@ import { resolversProject } from "../models/proyecto/ResolversProject";
 import { resolversUser } from "../models/usuario/ResolversUser";
 import {resolversInscripciones} from '../models/inscripcion/resolvers';
 import {resolversAvance} from '../models/avance/resolvers';
-
+// hola mundo
 export const resolvers = [ resolversProject, resolversUser, resolversInscripciones, resolversAvance ];
